9fbd7ffd74c2a8b59fd0ce4f5df1b3cec2834e29
Aditya Tripathi <adityatripathi.at04@gmail.com>
2024-09-09 03:14:00 +0530

    changed to run on a default port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
