c7e82ecee801f221ad14f7b1f8eefaeccee2f71e
Aditya Tripathi <adityatripathi.at04@gmail.com>
2024-09-09 03:23:39 +0530

    Refactor code to run on a default port and handle errors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
@@ -24,9 +24,4 @@ const runApp = async () => {
     });
 }
 
-try {
-    runApp();
-} catch (error) {
-    console.error('Error!! Restarting:', error);
-    runApp();
-}
+runApp();
